a5ca90c29d862da491219c0bf4c9f4548efc9f91
6704062611263 <s6704062611263@email.kmutnb.ac.th>
2024-11-26 14:57:22 +0700

    Add files via upload

---
+++ lab1minx.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int n1 = 15;
+	int n2 = 5;
+	
+	if ( n1 > n2 ){
+		printf("n1 is greater than n2");
+	} else{
+		printf("n2 is greater than n1");
+	}
+	return 0;
+}

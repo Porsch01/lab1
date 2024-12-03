f339f4af53adb1af5876cb0e5d78998bc71922fd
6704062611263 <s6704062611263@email.kmutnb.ac.th>
2024-12-03 13:17:44 +0700

    ข้อ 1 ครับ if if

--- lab1minx.c
+++ lab1minx.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-int main(){
-	int n1
-	,n2;
-	scanf("%d",&n1);
-	scanf("%d",&n2);
-	if ( n1 > n2 ){
+int main()
+{
+	int n1 = 15;
+	int n2 = 5;
+	
+	if( n1 > n2 ){
 		printf("n1 is greater than n2");
-		printf("\n%d",(n1-n2));
-	} else{
+	} 
+	if(n2 > n1){
 		printf("n2 is greater than n1");
-		printf("\n%d",(n2-n1));
 	}
 	return 0;
 }
